65511b37fb8617c08db66fbe60e8494920f41744
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2025-09-07 15:59:26 +0530

    feat: created star and arc logo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SimpleStar from './Components/Start'
+import SimpleT from './Components/ArcLogo'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-black'>
+      <div>
+
+      
+      <SimpleStar />
+      <SimpleT />
+      </div>
+    </div>
+  )
+}
+
+export default App
